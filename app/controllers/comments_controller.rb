d69f9de61445ddc37043a5614c76fcff8f41a9e6
Winnie Wu <32531807+WCW789@users.noreply.github.com>
2024-02-29 21:19:58 +0000

    Revert "Ajaxified comments, working on follows and likes"
    This reverts commit 371a404be7a04a16c8587891df39d307e7196625.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,7 +21,6 @@ class CommentsController < ApplicationController
   def edit
     respond_to do |format|
       format.html
-      format.js
     end
   end
 
@@ -34,10 +33,6 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
-        format.js
-        # format.js do
-        #   render template: "comments/create.js.erb"
-        # end
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -51,7 +46,6 @@ class CommentsController < ApplicationController
       if @comment.update(comment_params)
         format.html { redirect_to root_url, notice: "Comment was successfully updated." }
         format.json { render :show, status: :ok, location: @comment }
-        format.js
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -65,10 +59,6 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
-
-      format.js do
-        render template: "comments/destroy"
-      end
     end
   end
 
