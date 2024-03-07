770d3565c77124deab6e239cc62e46ab8fc3b1fc
Winnie Wu <32531807+WCW789@users.noreply.github.com>
2024-03-07 16:32:51 +0000

    made changes per code reviews

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -35,9 +35,10 @@ class CommentsController < ApplicationController
         format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
         format.js
-        # format.js do
-        #   render template: "comments/create.js.erb"
-        # end
+        # This is the more explicit way of writing this line:
+         # format.js do
+         #   render template: "comments/create.js.erb"
+         # end
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -65,7 +66,6 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
-
       format.js do
         render template: "comments/destroy"
       end
